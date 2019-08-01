e1bf400e09722aaf714e9990f8ad365a5cc9161b
sasha walker <sashawalker31@gmail.com>
2019-08-01 02:42:36 +0000

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,34 @@ require 'yaml'
 
 RSpec.configure do |config|
 
-  def __
-    # raise "replace underscore with code"
+  def new_hash
+    new_hash{}
   end
 
 end
+
+def my_hash
+  my_hash = { name: = "Sam"}
+end
+
+def pioneer
+  { :name = 'Grace Hopper'}
+end
+
+def id_generator
+  {:id = "2"}
+end
+
+def read_from_hash
+  person= { name:"Sam"}
+  person[:hometown]
+  person[:hometown]="New Orleans"
+end
+
+def update_counting_hash
+ person= {name:"Sarah"}
+ person[:race]
+ person[:race]="african american"
+ end
+  
+  
